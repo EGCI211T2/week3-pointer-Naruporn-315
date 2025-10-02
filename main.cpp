2e9c09e57de2550a3d18b06409442ad2708ced5d
Naruporn-315 <naruporn.kau@student.mahidol.edu>
2025-10-02 04:26:57 +0000

    completed

--- main.cpp
+++ main.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
 #include <iomanip>
-#define SIZE 10
-
 using namespace std;
 
 int main(int argc,char *argv[]){
   int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
-cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+int n;
+//ask for numbers of input
+cout<<"How many numbers needed : ";
+cin>>n;
+pa=new int[n]; 
 
-pa = &a[0];  
-pb = &a[SIZE-1];
-for (i=0; i<SIZE/2; i++)
-{
+//ask for the input
+for(i=0;i<n;i++,pa++){
+  cout<<"Input no."<<i+1<<" : ";
+  cin>>*pa;
+}
+pa=pa-n; //pa[0]
+
+//print original
+cout<<"Original: ";
+for (i=0; i<n-1; i++ ,pa++){
+   cout<<setw(3)<<*pa;
+}
+cout<<setw(3)<<*pa<<endl; //print the last one
+
+// reverse
+pb = pa; //pa[n]
+pa -= (n-1); //pa[0]
+for (i=0; i<n/2; i++){
    temp = *pa;  
    *pa = *pb;  
    *pb = temp;
    pa++;  pb--;
 }
-pa=&a[0];
-pb-=SIZE/2;
+// print reverse
+pa -= n/2;
 cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
+for (i=0; i<n-1; i++ ,pa++){
+   cout<<setw(3)<<*pa; 
 }
-cout<<setw(3)<<*pa<<endl;
+cout<<setw(3)<<*pa<<endl; //print the last one
 
-
+//delete []pa;
 return 0;
 }
